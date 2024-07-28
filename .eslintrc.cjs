04fa3de940fefa1ba4ef4904d8ca231ea42b298b
Abhishek Kumar <161105067+abhishekKr-007@users.noreply.github.com>
2024-07-28 19:29:55 +0530

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,15 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-      'react-app',
-    'plugin:react-refresh/recommended'
+    'react-app'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -16,7 +18,7 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+      { allowConstantExport: true }
+    ]
+  }
+};
