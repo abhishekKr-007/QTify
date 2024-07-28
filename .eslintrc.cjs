78490628b3321e6f08e0e1c9b5c85579d2b978df
Abhishek Kumar <161105067+abhishekKr-007@users.noreply.github.com>
2024-07-28 21:41:06 +0530

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,13 +9,16 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
-    'react-app'
+    'react-app',
+    "react-app", "react-app/jest"
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': 'warn'
+    'react-refresh/only-export-components': 'warn',
+     "react/prop-types": "off",
+        "react/no-unescaped-entities": "off"
   }
 };
